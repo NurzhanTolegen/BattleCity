770106c81e32d2404a519d6b2308a804897ec5b5
NurzhanTolegenLaptop <nurzzz27@gmail.com>
2021-07-21 23:12:55 +0600

    тряска экрана

--- Assets/Scripts/EffectsController.cs
+++ Assets/Scripts/EffectsController.cs
@@ -16,6 +16,12 @@ public class EffectsController : MonoBehaviour
     [Header("Spawn")]
     public ParticleSystem spawnParticles;
 
+    [Header("Shake")]
+    public Transform cam;
+    public float shakeRange = 0.1f;
+    public float shakePeriod = 0.1f;
+    public int shakeCount = 10;
+
     private void Awake() {
         instance = this;
         
@@ -25,6 +31,8 @@ public class EffectsController : MonoBehaviour
         instance.boomParticles.transform.position = position;
         instance.boomParticles.Play();
         instance.boomSound.Play();
+
+        instance.StartCoroutine(instance.HitShakeEffectCoroutine());
     }
     public static void PlayShootEffect() {
         instance.shootSound.Play();
@@ -34,4 +42,16 @@ public class EffectsController : MonoBehaviour
         instance.spawnParticles.Play();
     }
 
+    IEnumerator HitShakeEffectCoroutine() {
+        for (int i = 0; i < shakeCount; i++) {
+            yield return new WaitForSecondsRealtime(shakePeriod);
+
+            Vector3 offset = Vector3.zero;
+            offset.x = Random.Range(-shakeRange, shakeRange);
+            offset.y = Random.Range(-shakeRange, shakeRange);
+            offset.z = Random.Range(-shakeRange, shakeRange);
+            cam.localPosition = offset;
+        }
+        cam.localPosition = Vector3.zero;
+    }
 }
